4518d08043707fe5764e78d450fd8015ef11b1e6
cgnerds <cgnerds@gmail.com>
2018-08-20 19:27:27 +0800

    Chapter 1 Introducinng Machine Learning and ML-Agents.

---
+++ unity-environment/Assets/Simple/Scripts/SimpleAgent.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class SimpleAgent : Agent {
+
+
+
+    public override void CollectObservations()
+    {
+        AddVectorObs(0);
+    }
+
+    public Bandit bandit;
+    public override void AgentAction(float[] vectorAction, string textAction)
+	{
+        var action = (int)vectorAction[0];
+        AddReward(bandit.PullArm(action));
+        // Done();
+    }
+
+    public override void AgentReset()
+    {
+        bandit.Reset();
+    }
+
+    public override void AgentOnDone()
+    {
+
+    }
+
+    public Academy academy;
+    public float timeBetweenDesicionAtInference;
+    private float tiemSinceDecision;
+
+    public void FixedUpdate()
+    {
+        WaitTimeInference();
+    }
+
+    private void WaitTimeInference()
+    {
+        if(!academy.GetIsInference())
+        {
+            RequestDecision();
+        }
+        else
+        {
+            if(tiemSinceDecision >= timeBetweenDesicionAtInference)
+            {
+                tiemSinceDecision = 0f;
+                RequestDecision();
+            }
+            else
+            {
+                tiemSinceDecision += Time.fixedDeltaTime;
+
+            }
+        }
+    }
+}
